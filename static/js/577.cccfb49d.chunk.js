9355f33e267877fdff627aab900cf83dc64c6d34
byht1 <99541944+byht1@users.noreply.github.com>
2023-01-03 19:55:40 +0000

    Deploying to gh-pages from @ byht1/patissier-client@9946b6f8bfa9a08c80d5dc5dbcacaee660a7091b 🚀

---
+++ static/js/577.cccfb49d.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkpatissier_client=self.webpackChunkpatissier_client||[]).push([[577],{577:function(e,n,t){t.r(n),t.d(n,{default:function(){return s}});t(791);var i=t(329);function s(){return(0,i.jsx)("div",{})}}}]);
+//# sourceMappingURL=577.cccfb49d.chunk.js.map

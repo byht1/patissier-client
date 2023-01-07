42e581bad55a0037e423cb852ae5c26922dd17b7
byht1 <vitalikbyht1@gmail.com>
2023-01-07 21:48:29 +0200

    fix

--- src/page/Home/Home.jsx
+++ src/page/Home/Home.jsx
@@ -1,12 +1,8 @@
-import { TitleH2 } from 'components/global/text';
 import React from 'react';
 
 export default function Home() {
   return (
     <div>
-      <TitleH2 mt={5} ml={10} color="good">
-        111111111111
-      </TitleH2>
       {/* Це лише приклад назви можуть бути інші */}
       {/* <Hero /> */}
       {/* <Statistics /> */}

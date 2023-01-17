2139619a7535bf1b44037ae7400e42515a3210e8
ViktoriaZinyak <100145684+ViktoriaZinyak@users.noreply.github.com>
2023-01-17 13:08:58 +0200

    no overflow

--- src/components/modules/Reviews/ReviewsItem/ReviewItem.jsx
+++ src/components/modules/Reviews/ReviewsItem/ReviewItem.jsx
@@ -23,12 +23,7 @@ export const ReviewItem = ({ reviewItem }) => {
             <p>{formatedDate}</p>
           </div>
         </ReviewData>
-        <ReviewComment>
-          {reviewItem.comments}
-          Дівчата, ці тістечка посто бомба-ракєта!!! Нарешті дійшов мій
-          марципан, і я одразу приготувала, тому тепер я в графіку і в ногу з
-          іншими учасниками курсу.
-        </ReviewComment>
+        <ReviewComment>{reviewItem.comments}</ReviewComment>
       </ReviewWrap>
     </>
   );

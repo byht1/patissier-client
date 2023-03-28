65439f4b22197c5cb603fed197fe4eebee3ac407
KautTanya <kaut.tanya.s@gmail.com>
2023-03-28 13:15:39 +0300

    start

--- src/page/MasterClasses/MasterClasses.jsx
+++ src/page/MasterClasses/MasterClasses.jsx
@@ -1,5 +1,22 @@
 import React from 'react';
+import { Reviews } from 'components/modules/Reviews/Review';
+import { Box } from 'components/global/Box';
+import { Container } from 'components/global/Container';
+import { TitleH2, Text } from 'components/global/text';
 
 export default function MasterClasses() {
-  return <div>MasterClasses</div>;
+  return (
+  <>
+    <Container pt={58} pb={200}>
+
+      <Box display="flex" justifyContent="center" flexDirection="column" alignItems="center">
+        <TitleH2 mb={52} color="at">
+            Різдвяна випічка
+        </TitleH2>
+        <Text color="t" mb={56} width={558} textAlign="center" lh='big'>Пройдіть майстер-клас та навчіться готувати неймовірно смачний різдвяний штолен</Text>
+          <Reviews />
+      </Box>
+    </Container>
+    </>
+    )
 }

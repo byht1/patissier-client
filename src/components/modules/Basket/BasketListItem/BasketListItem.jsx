066f75973117c2f9d94fa711c64560437dee568e
zlatan-dp <zlatandnepr@gmail.com>
2023-04-06 16:51:49 +0300

    product element layout

---
+++ src/components/modules/Basket/BasketListItem/BasketListItem.jsx
@@ -0,0 +1,78 @@
+import React from 'react';
+import {
+  ImgWrap,
+  ItemPhoto,
+  ItemWrap,
+  ItemInfoWrap,
+  ProductTitle,
+  Text,
+  PriceWrap,
+  PriceValue,
+  CounterWrap,
+  CounterBtn,
+  CounterValue,
+  DelBtn,
+  Svg,
+  TotalPrice,
+} from './BasketListItem.styled';
+
+import Image from 'img/basket/tart.png';
+import DelIcon from 'img/basket/icons.svg';
+import { Box } from 'components/global/Box';
+
+export const BasketListItem = () => {
+  return (
+    <>
+      <ItemWrap>
+        <ImgWrap>
+          <ItemPhoto src={Image} alt="product foto" loading="lazy" />
+        </ImgWrap>
+        <ItemInfoWrap>
+          <ProductTitle>Тарт з ганашем</ProductTitle>
+          <Box
+            display="flex"
+            justifyContent="space-between"
+            alignItems="center"
+            mb="20px"
+          >
+            <Text>Ціна</Text>
+            <PriceWrap>
+              <PriceValue>48,50</PriceValue>
+            </PriceWrap>
+          </Box>
+          <Box
+            display="flex"
+            justifyContent="space-between"
+            alignItems="center"
+            mb="60px"
+          >
+            <Text>Кількість</Text>
+            <CounterWrap>
+              <CounterBtn>-</CounterBtn>
+              <CounterValue>1</CounterValue>
+              <CounterBtn>+</CounterBtn>
+            </CounterWrap>
+          </Box>
+          <Box
+            display="flex"
+            justifyContent="space-between"
+            alignItems="center"
+          >
+            <DelBtn>
+              <Svg
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <use xlinkHref={DelIcon + '#icon-del'} />
+              </Svg>
+            </DelBtn>
+            <TotalPrice>48,50</TotalPrice>
+          </Box>
+        </ItemInfoWrap>
+      </ItemWrap>
+    </>
+  );
+};
--- src/page/Basket/Basket.jsx
+++ src/page/Basket/Basket.jsx
@@ -1,5 +1,15 @@
 import React from 'react';
 
+import { BackLinkRef } from 'components/global/BackLink/BackLink';
+import { BasketTitle } from 'components/modules/Basket/BasketTitle';
+import { BasketListItem } from 'components/modules/Basket/BasketListItem';
+
 export default function Basket() {
-  return <div>Basket</div>;
+  return (
+    <>
+      <BackLinkRef />
+      <BasketTitle />
+      <BasketListItem />
+    </>
+  );
 }

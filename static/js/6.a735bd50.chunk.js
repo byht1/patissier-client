0bd69c7d74ba71085e7a28afc7fe277cbd42883a
VadimLuginets <VadimLuginets@users.noreply.github.com>
2023-04-08 12:55:23 +0000

    Deploying to gh-pages from @ byht1/patissier-client@0df4e5afb0d5d49fc4f67c8e6823adeb10d16ff2 🚀

--- static/js/6.a735bd50.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkpatissier_client=self.webpackChunkpatissier_client||[]).push([[6],{5006:function(e,n,t){t.r(n),t.d(n,{default:function(){return s}});t(2791);var i=t(3329);function s(){return(0,i.jsx)("div",{children:"Basket"})}}}]);
-//# sourceMappingURL=6.a735bd50.chunk.js.map

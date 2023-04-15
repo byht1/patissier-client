ef820f46b8735ea84c08abb42afe393bd4d31dde
zlatan-dp <zlatan-dp@users.noreply.github.com>
2023-04-15 08:02:01 +0000

    Deploying to gh-pages from @ byht1/patissier-client@870babf30258d74e3d75b2c5f046f5da520c0e49 🚀

---
+++ static/js/679.1456b5bf.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkpatissier_client=self.webpackChunkpatissier_client||[]).push([[679],{5679:function(e,s,n){n.r(s),n.d(s,{default:function(){return i}});n(2791);var t=n(3329);function i(){return(0,t.jsx)("div",{children:"MasterClasses"})}}}]);
+//# sourceMappingURL=679.1456b5bf.chunk.js.map

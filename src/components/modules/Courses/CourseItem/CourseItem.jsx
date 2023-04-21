81874fedfd0e953bf2090b1ba6aebe5a38faea09
Veronika Kravchenko <aniregma@gmail.com>
2023-04-21 23:46:49 +0300

    updated styles

--- src/components/modules/Courses/CourseItem/CourseItem.jsx
+++ src/components/modules/Courses/CourseItem/CourseItem.jsx
@@ -1,58 +1,50 @@
 import {
   CourseCard,
-  ImageList,
   ImageWpar,
   CourseImage,
   InfoList,
   InfoItem,
   CardFooter,
+  ToDetailsLink,
 } from './CourseItem.styled';
 
-import Image from 'img/courses/courseImg.jpg';
+import Image from 'img/courses/випічка.jpg';
 import { Text, TitleH4 } from 'components/global/text';
-import { ButtonsGhost } from 'components/global/button';
+import { Box } from 'components/global/Box';
 
 export const CourseItem = () => {
+  const type = 'курc';
+  const navigationPath = type === 'курс' ? 'current-courses' : 'master-classes';
+
   return (
     <CourseCard>
-      <TitleH4 textAlign="center" color="at" mb={24}>
+      <ImageWpar>
+        <CourseImage src={Image} alt="" loading="lazy" />
+      </ImageWpar>
+      <Box padding="20px">
+      <TitleH4 textAlign="center" color="wt" mb={8}>
         Випічка
       </TitleH4>
-      <ImageList>
-        <ImageWpar>
-          <CourseImage src={Image} alt="" loading="lazy" />
-        </ImageWpar>
-        <ImageWpar>
-          <CourseImage src={Image} alt="" loading="lazy" />
-        </ImageWpar>
-      </ImageList>
-      <Text mb={24} lh="body">
+      <Text mb={20} lh="body" textAlign="center" color="t">Курси</Text>
+      <Text mb={12} lh="body" color="t">
         На курсі ви навчитеся працювати з різними видами тіста, створювати
         круасани, сінабони та запашні булочки
       </Text>
       <InfoList>
         <InfoItem>
-          <Text weight={700} size={16} lh="big">
+          <Text weight={700} size={16} lh="heading">
             Група:
           </Text>
-          <Text weight={400} size={20} lh="big">
+          <Text weight={400} size={16} lh="heading">
             15 осіб
           </Text>
         </InfoItem>
         <InfoItem>
-          <Text weight={700} size={16} lh="big">
+          <Text weight={700} size={16} lh="heading">
             Термін:
           </Text>
-          <Text weight={400} size={20} lh="big">
+          <Text weight={400} size={16} lh="heading">
             5 днів
-          </Text>
-        </InfoItem>
-        <InfoItem>
-          <Text weight={700} size={16} lh="big">
-            Старт:
-          </Text>
-          <Text weight={400} size={20} lh="big">
-            15 грудня
           </Text>
         </InfoItem>
       </InfoList>
@@ -60,8 +52,9 @@ export const CourseItem = () => {
         <Text weight={600} size={20} lh="big">
           10 000грн.
         </Text>
-        <ButtonsGhost p="16px 40px">Детальніше</ButtonsGhost>
+        <ToDetailsLink to={`/${navigationPath}`}>Детальніше</ToDetailsLink>
       </CardFooter>
+      </Box>
     </CourseCard>
   );
 };

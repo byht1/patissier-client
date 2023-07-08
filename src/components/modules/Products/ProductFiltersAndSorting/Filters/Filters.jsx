2fd7bfd47d4e2b124387e5a182a0a1a17458195e
ViktoriaZinyak <100145684+ViktoriaZinyak@users.noreply.github.com>
2023-07-08 16:01:14 +0300

    input min max number

--- src/components/modules/Products/ProductFiltersAndSorting/Filters/Filters.jsx
+++ src/components/modules/Products/ProductFiltersAndSorting/Filters/Filters.jsx
@@ -220,48 +220,149 @@ const CustomSlider = styled(Slider)(({ theme }) => ({
 //   { name: 'MuiSlider' }
 // );
 
+// export const Filters = () => {
+//   const [val, setVal] = React.useState({ min: 0, max: 100 });
+//   const [val2, setVal2] = React.useState([1, 2300]);
+//   const getValue = () => {
+//     const value = val2[0].startsWith('0') ? val2[0].slice(1) : val2[0];
+//     return value;
+//   };
+//   const handleInputChange = e => {
+//     //   setVal2([val2[0], Number(e.target.value)]);
+//     let inputValue = Number(e.target.value);
+//     // if (inputValue.toString().startsWith('0')) {
+//     //   inputValue = parseInt(inputValue, 10);
+//     // }
+//     if (inputValue > 2300) {
+//       inputValue = 2300;
+//     }
+//     if (inputValue < 1) {
+//       inputValue = '';
+//     }
+//     setVal2([val2[0], inputValue]);
+//     console.log(val2[1]);
+//   };
+//   return (
+//     <div className="App" style={{ maxWidth: '100vw', padding: '0 10vw' }}>
+//       <p>Ціна</p>
+//       <div>
+//         <div>
+//           <CustomSlider
+//             //disabled
+//             // classes={classes}
+//             step={1}
+//             min={1}
+//             max={2300}
+//             value={val2}
+//             onChange={(ev, v) => setVal2(v)}
+//             onChangeCommitted={(ev, v) => console.log(v)}
+//             valueLabelDisplay="off"
+//             aria-labelledby="range-slider"
+//           />
+//           <input
+//             type="number"
+//             min="1"
+//             max="2300"
+//             value={val2[0]}
+//             onChange={e => {
+//               console.log(e.target.value);
+//               const value = e.target.value.startsWith('0')
+//                 ? e.target.value.slice(1)
+//                 : e.target.value;
+//               console.log(value);
+//               setVal2([Number(value), val2[1]]);
+//               console.log(val2[0]);
+//             }}
+//           />
+//           <input
+//             type="number"
+//             min="1"
+//             max="2300"
+//             value={val2[1]}
+//             onChange={handleInputChange}
+//             // onKeyDown={e => {
+//             //   if (e.target.value >= 2300 && e.key !== 'Backspace') {
+//             //     e.preventDefault();
+//             //   }
+//             // }}
+//           />
+//         </div>
+//         <div
+//           style={{
+//             display: 'flex',
+//             alignItems: 'center',
+//             justifyContent: 'space-between',
+//             margin: '1rem 0',
+//             fontWeight: 500,
+//             color: '#8f23b3',
+//           }}
+//         >
+//           <div>{`€${val2[0]}`}</div>
+//           <div>{`€${val2[1]}`}</div>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// };
+
 export const Filters = () => {
   const [val, setVal] = React.useState({ min: 0, max: 100 });
-  const [val2, setVal2] = React.useState([0, 100]);
+  const [val2, setVal2] = React.useState(['1', '2300']);
+  const sliderVal = val2.map(Number);
+
+  const handleMinInputChange = e => {
+    let value = e.target.value.replace(/\D/g, '');
+    if (Number(value) >= Number(val2[1])) {
+      value = (Number(val2[1]) - 1).toString();
+    }
+    // if (value === '') {
+    //   value = '1';
+    //   console.log(value);
+    // }
+    setVal2([value, val2[1]]);
+  };
+  const handleBlur = e => {
+    let value = e.target.value.trim();
+    if (value === '') {
+      value = '1';
+      console.log(value);
+      e.target.value = value;
+    }
+    setVal2([value, val2[1]]);
+  };
+
+  const handleMaxInputChange = e => {
+    let value = e.target.value.replace(/\D/g, '');
+    if (Number(value) > 2300) {
+      value = '2300';
+    }
+    if (Number(value) < Number(val2[0])) {
+      value = (Number(val2[0]) + 1).toString();
+    }
+    setVal2([val2[0], value]);
+  };
+
   return (
     <div className="App" style={{ maxWidth: '100vw', padding: '0 10vw' }}>
       <p>Ціна</p>
       <div>
         <div>
           <CustomSlider
-            //disabled
-            // classes={classes}
             step={1}
-            min={0}
+            min={1}
             max={2300}
-            value={val2}
-            onChange={(ev, v) => setVal2(v)}
-            onChangeCommitted={(ev, v) => console.log(v)}
+            value={sliderVal}
+            onChange={(ev, v) => setVal2(v.map(String))}
             valueLabelDisplay="off"
             aria-labelledby="range-slider"
           />
           <input
-            type="number"
-            min="0"
-            max="2300"
+            type="text"
             value={val2[0]}
-            onChange={e => setVal2([Number(e.target.value), val2[1]])}
+            onChange={handleMinInputChange}
+            onBlur={handleBlur}
           />
-          <input
-            // type="number"
-            min=""
-            max="2300"
-            value={val2[1]}
-            onChange={e => {
-              setVal2([val2[0], Number(e.target.value)]);
-              console.log(val2);
-            }}
-            onKeyDown={e => {
-              if (e.target.value >= 2300 && e.key !== 'Backspace') {
-                e.preventDefault();
-              }
-            }}
-          />
+          <input type="text" value={val2[1]} onChange={handleMaxInputChange} />
         </div>
         <div
           style={{
